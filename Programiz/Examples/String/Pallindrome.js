a1678419140f0a0c1a7651206a7f6537ddf2ce5f
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-02-11 09:08:48 +0000

    pallindrome finished

--- Programiz/Examples/String/Pallindrome.js
+++ Programiz/Examples/String/Pallindrome.js
@@ -3,23 +3,43 @@
 
 // A string is a pallindrome if it is read the same forward or backward. ex. 'dad' , 'Racecar' , 'Madam' etc ..
 
+
+
 function checkPallindrome(string){
+   
+    // first convert the input into array coz string is primitive and can't be updated
 
-    let newString=''; // declared an empty string to store the string in reverse order 
-     
-  
-    for ( let i =string.length-1 ; i >=0 ; i--){
-        newString+=string[i]
+    let arrayOfWords = string.split("");
+    
+
+    for (let index=0; index<arrayOfWords.length; index++){
+        arrayOfWords[index]=arrayOfWords[index].toLowerCase();
     }
+     let newString = arrayOfWords.join('');
 
+     let reverseArray=[];
 
-    let output = (newString===string) ? `Given string "${string}" is a Pallindrome` : `Given string "${string}" is not a Pallindrome` ;
+     for (let index=arrayOfWords.length-1; index>=0; index--){
+        reverseArray.push(arrayOfWords[index])
+     }
+
+     let reversestring = reverseArray.join("")  
+    
+  
+    let output = (newString===reversestring) ? `Given string "${string}" is a Pallindrome` : `Given string "${string}" is not a Pallindrome` ;
 
     return output ;
 
+    
 }
 
-// take output from user
-const string = prompt('Enter the string to check wheater it is a pallindrome or not :' );
+let input = prompt('Enter the string to check wheater it is a pallindrome or not :');
 
-console.log(checkPallindrome(string));
+ while(!input){
+    input = prompt('Enter the string to check wheater it is a pallindrome or not :')
+ }
+
+console.log(checkPallindrome(input));
+
+
+

611aea45559d63c8477cc72feb56799f1493ee3f
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-02-12 06:03:05 +0000

    added mountain array

---
+++ Programiz/Examples/String/alphabetical_order.js
@@ -0,0 +1,14 @@
+// write a program to sort words in alphabetical order 
+
+// take user input 
+
+const input = prompt('Enter a sentence to arrange its words into alphabetical order :');
+
+while(!input) {
+    input = prompt('Enter a sentence to arrange its words into alphabetical order :');
+}
+
+function sortIntoAlphabeticalOrder(input){
+
+  
+}

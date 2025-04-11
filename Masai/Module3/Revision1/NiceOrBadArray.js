c4ba6332eee3a930c50f63b682db5f2334287fcf
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-04-11 10:59:37 +0000

    add somethig

--- Masai/Module3/Revision1/NiceOrBadArray.js
+++ Masai/Module3/Revision1/NiceOrBadArray.js
@@ -3,19 +3,18 @@ if the count of even elements in the array is greater than the value stored in K
 */
 
 function niceArray(N, array, K) {
-    //write code here
-    
-    let count = 0 ;
-    for ( let i = 0 ; i < N ; i++){
-      if (array[i]%2==0){
-        count++;
-      }
-    } ;
-    
-    if ( count > K) {
-      console.log('Nice Array')
-    } else {
-      console.log('Bad Array')
-    }
-  }
+  //write code here
   
+  let count = 0 ;
+  for ( let i = 0 ; i < N ; i++){
+    if (array[i]%2==0){
+      count++;
+    }
+  } ;
+  
+  if ( count > K) {
+    console.log('Nice Array')
+  } else {
+    console.log('Bad Array')
+  }
+}
